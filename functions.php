4d33e3fb6a8a49a56cd9df56b0376eed31a933e7
codestickdev <35964064+codestickdev@users.noreply.github.com>
2021-07-30 22:54:53 +0200

    ACF Json

--- functions.php
+++ functions.php
@@ -199,6 +199,20 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
 
+/* ACF local JSON */
+add_filter('acf/settings/save_json', 'my_acf_json_save_point');
+function my_acf_json_save_point( $path ) {
+    $path = get_stylesheet_directory() . '/acf-json';
+    return $path;
+}
+
+add_filter('acf/settings/load_json', 'my_acf_json_load_point');
+function my_acf_json_load_point( $paths ) {
+    unset($paths[0]);
+    $paths[] = get_stylesheet_directory() . '/acf-json';
+    return $paths;
+}
+
 /*
  *	Contact form
  */
